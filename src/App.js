985d139bd2a17004997b2ded1b6a8fd6a29cf8c5
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-11-28 23:15:29 +0630

    Initinal Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import * as JSZip from 'jszip';
+axios.get(
+    "https://hichculturedmm.s3.ap-southeast-1.amazonaws.com/formData.zip",
+    { 
+      headers: { 
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET',
+      },
+      responseType: "blob" 
+    })
+    .then((response) => {
+
+        let zip = new JSZip();
+        zip.loadAsync(response.data).then((value) => {
+            console.log('jszip unzipped response.data');
+            value.forEach((path, file) => {
+                console.log(path);
+            })
+        }).catch((e) => {
+            console.log(`jszip failed on response.data: ${e}`);
+        })
+
+        let buffer = Buffer.from(response.data, 'binary');
+        zip.loadAsync(buffer).then((value) => {
+            console.log('jszip unzipped buffer');
+            value.forEach((path, file) => {
+                console.log(path);
+            })
+        }).catch((e) => {
+            console.log(`jszip failed on buffer: ${e}`);
+        })
+
+    }).catch((reason) => {
+        console.log(`axios request failed: ${reason}`);
+    })
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    </>
   );
 }
 
